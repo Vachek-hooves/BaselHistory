2fc5bf33757fda77e4254a6923b128da55abaef8
Vachek-hooves <vachek0602@gmail.com>
2024-08-05 21:08:12 +0300

    done:Book Screen

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,15 +1,43 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {MainBg, MainLayout} from '../components/layout';
 import {COLORS} from '../constant/colors';
+import {book} from '../data/bookOfKnoladge';
 
 const BookScreen = () => {
   return (
     <MainBg>
-      <MainLayout style={{backgroundColor: COLORS.beige + 70}}></MainLayout>
+      <MainLayout style={{backgroundColor: COLORS.black + 90}}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{padding: 10}}>
+          {book.map((story, i) => (
+            <View key={i}>
+              <View>
+                <Text style={styles.header}>{story.topic}</Text>
+                <Text style={styles.text}>{story.text}</Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </MainLayout>
     </MainBg>
   );
 };
 
 export default BookScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 26,
+    textAlign: 'center',
+    marginVertical: 30,
+    fontWeight: '600',
+    color:COLORS.mandarin
+  },
+  text: {
+    fontSize: 18,
+    textAlign: 'justify',
+    color: COLORS.beige,
+    fontWeight: '600',
+  },
+});
